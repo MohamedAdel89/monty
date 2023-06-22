9594280e26509ffa6ce15c4daf8b8475ff570b83
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-06-21 19:47:26 -0700

    FD:

--- main.c
+++ main.c
@@ -1,167 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include "monty.h"
-carrier_t carrier = {0, NULL, NULL, NULL, 0};
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- *main - entry point
- *@argc: number of arguments
- *@argv: arguments
- *Return: 0 always
- */
-
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	size_t line_size = 0;
-	stack_t *dlinkedlist = NULL;
-	unsigned int line_num = 0;
-	void (*cmd)(stack_t **stack, unsigned int line_number) = NULL;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
-	{       fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);    }
-	carrier.stream = fopen(argv[1], "r");
-	if (carrier.stream == NULL)
-	{       fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);    }
-	while (getline(&(carrier.line), &line_size, carrier.stream) != -1)
 	{
-		line_num++;
-		carrier.words = split(carrier.line, " \n\t");
-		if (carrier.words != NULL && carrier.words[0][0] != '#')
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
 		{
-			if (check_if_not_num(carrier.words[1]) == -1 &&
-				strcmp("push", carrier.words[0]) == 0)
-			{       fprintf(stderr, "L%i: usage: push integer\n", line_num);
-				goto free_all;     }
-			carrier.data = (carrier.words[1] == NULL) ? 0 :
-				(unsigned int) atoi(carrier.words[1]);
-			cmd = get_op(carrier.words[0], line_num);
-			if (cmd == NULL)
-				goto free_all;
-			cmd(&dlinkedlist, line_num);
-			free(carrier.words);
+			execute(content, &stack, counter, file);
 		}
-		else
-			free(carrier.words);
-		free(carrier.line);
-		carrier.line = NULL;
+		free(content);
 	}
-	free(carrier.line), free_dlistint(dlinkedlist), fclose(carrier.stream);
-	return (EXIT_SUCCESS);
-free_all:
-	free(carrier.line), free(carrier.words), free_dlistint(dlinkedlist);
-	fclose(carrier.stream), exit(EXIT_FAILURE);
-}
-
-/**
- *split - splits a string according to the delimiter
- *@str: pointer to the string
- *@delim: pointer to the delimiter
- *Return: an array with each word
- */
-
-char **split(char *str, const char *delim)
-{
-	char **argv, *buffer, *tmpCpy = strdup(str);
-	unsigned int wNum = 0, i = 0;
-
-	if (*str == '\0')
-	{
-		free(tmpCpy);
-		return (NULL);
-	}
-	/*Count the number of words to allocate memory*/
-	buffer = strtok(tmpCpy, delim);
-	wNum += 1;
-	while (buffer != NULL)
-	{
-		buffer = strtok(NULL, delim);
-		if (buffer != NULL)
-			wNum += 1;
-	}
-	free(tmpCpy);
-
-	/*Allocate memory and check if it couldn't*/
-	argv = malloc(sizeof(char *) * (wNum + 1));
-	if (argv == NULL)
-		return (NULL);
-
-	/*Save pointer in the bidimensional array*/
-	buffer = strtok(str, delim);
-	if (buffer != NULL)
-		*(argv) = buffer;
-	else
-	{
-		free(argv);
-		return (NULL);
-	}
-
-	for (i = 1; i < wNum; i++)
-		*(argv + i) = strtok(NULL, delim);
-	*(argv + i) = NULL;
-	return (argv);
-}
-
-/**
- * get_op - gets function
- * @command: command to check for
- * @LN: line number for error output
- * Return: none
- */
-
-void (*get_op(char *command, LN))(stack_t **stack, unsigned int line_number)
-{
-	int i = 0;
-	instruction_t ops[] = {
-		{"push", op_push},
-		{"pall", op_pall},
-		{"pint", op_pint},
-		{"pop", op_pop},
-		{"swap", op_swap},
-		{"nop", op_nop},
-		{"div", op_div},
-		{"mod", op_mod},
-		{"add", op_add},
-		{"pchar", op_pchar},
-		{"sub", op_sub},
-		{"mul", op_mul},
-		{"stack", op_stack},
-		{"queue", op_queue},
-		{"pstr", op_pstr},
-		{"rotl", op_rotl},
-		{"rotr", op_rotr},
-		{NULL, NULL}
-	};
-
-	while (ops[i].opcode != NULL)
-	{
-		if (strcmp(command, ops[i].opcode) == 0)
-			return (ops[i].f);
-		i++;
-	}
-	fprintf(stderr, "L%i: unknown instruction %s\n", line_number, command);
-	return (NULL);
-}
-
-/**
- *check_if_not_num - checks if a string is a number
- *@str: string to be checked
- *Return: -1 if not num 1 if it is a num
-*/
-
-int check_if_not_num(char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (-1);
-	for (i = 0; *(str + i) != '\0'; i++)
-	{
-		if (isdigit(*(str + i)) == 0 && *str != '-')
-			return (-1);
-	}
-	return (1);
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
--- monty.h
+++ monty.h
@@ -1,29 +1,12 @@
-#ifndef __MONTY__
-#define __MONTY__
-
+#ifndef MONTY_H
+#define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
-#define LN unsigned int line_number
-
-/**
- *struct carrier_s - auxiliar structure
- *@data: data to be inserted into stack or queue
- *@words: pointer to be freed
- *@line: pointer to be free
- *@stream: file to be closed if one fails
- *@state: indicates whether is a stack (0) or a queue (1)
- */
-
-typedef struct carrier_s
-{
-	int data;
-	char **words;
-	char *line;
-	FILE *stream;
-	int state;
-} carrier_t;
-
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -33,14 +16,28 @@ typedef struct carrier_s
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct stack_s
 {
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -49,57 +46,33 @@ typedef struct stack_s
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
-
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, LN);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-/*Global Variables*/
-extern carrier_t carrier;
-
-/*Prototypes*/
-
-/*dlinkedlist#1*/
-void free_dlistint(stack_t *head);
-size_t dlistint_len(const stack_t *h);
-size_t print_dlistint(const stack_t *h);
-stack_t *add_dnodeint(stack_t **head, int n);
-void delete_head(stack_t **dlistint_t);
-
-/*dlinkedlist#2*/
-stack_t *rev_dlistint(stack_t **h);
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-
-/*monty_ops#1*/
-void op_push(stack_t **stack, unsigned int line);
-void op_pall(stack_t **dlinkedlist, unsigned int line);
-void op_pint(stack_t **dlinkedlist, unsigned int line);
-void op_swap(stack_t **dlinkedlist, unsigned int line);
-void op_pop(stack_t **dlinkedlist, unsigned int line_num);
-
-/*monty_ops#2*/
-void op_nop(stack_t **dlinkedlist, unsigned int line);
-void op_div(stack_t **dlinkedlist, unsigned int line);
-void op_mod(stack_t **dlinkedlist, unsigned int line);
-void op_add(stack_t **dlinkedlist, unsigned int line_num);
-void op_pchar(stack_t **dlinkedlist, unsigned int line_num);
-
-/*monty_ops#3*/
-void op_sub(stack_t **dlinkedlist, unsigned int line_num);
-void op_mul(stack_t **dlinkedlist, unsigned int line_num);
-void op_stack(stack_t **dlinkedlist, unsigned int line_num);
-void op_queue(stack_t **dlinkedlist, unsigned int line_num);
-void op_pstr(stack_t **dlinkedlist, unsigned int line_num);
-
-/*monty_ops#4*/
-void op_rotl(stack_t **dlinkedlist, unsigned int line_num);
-void op_rotr(stack_t **dlinkedlist, unsigned int line_num);
-
-/*main*/
-char **split(char *str, const char *delim);
-void (*get_op(char *command, LN))(stack_t **stack, LN);
-int check_if_not_num(char *str);
-
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+void free_stack(stack_t *head);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 #endif
